ca63a544c2d27c6d2d7dee2d9450f1052779d7fb
JeronimoLa <81096816+JeronimoLa@users.noreply.github.com>
2025-02-23 15:44:53 -0600

    config fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://jeronimola.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/JeronimoLa.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
