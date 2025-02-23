d70779cec8221508a2578616ad41e9a4fd29161c
JeronimoLa <81096816+JeronimoLa@users.noreply.github.com>
2025-02-23 16:26:57 -0600

    deploy fix to yml file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'JeronimoLa', // Usually your GitHub org/user name.
   projectName: 'JeronimoLa.github.io/', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
