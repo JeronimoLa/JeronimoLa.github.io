6efa08c4bce4ca7769d366d5f67d2e01c0fb0f8f
JeronimoLa <81096816+JeronimoLa@users.noreply.github.com>
2025-02-23 16:03:10 -0600

    deploy gh pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://jeronimola.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/JeronimoLa.github.io/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'JeronimoLa', // Usually your GitHub org/user name.
+  projectName: 'JeronimoLa.github.io/', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
